38a528c0b36442173c5128578e996c36e931af90
naoto4420-pixel <naoto4420@gmail.com>
2026-01-28 20:06:01 +0900

    ブックマーク画像表示追加

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,5 +5,14 @@ class Link < ApplicationRecord
 
   # バリデーション
   validates :url, presence: true
-  
+
+  # 画像リサイズ用のメソッド
+  ## 呼び出し時に variant(:thumb) などでサイズ指定できるようにする
+  def thumbnail_variant
+    return unless thumbnail.attached?
+    
+    # 300x200のサイズに収まるようにリサイズ（要ImageMagick/Vips）
+    # ※エラーが出る場合は一旦 thumbnail をそのまま返す
+    thumbnail.variant(resize_to_limit: [300, 200])
+  end
 end
