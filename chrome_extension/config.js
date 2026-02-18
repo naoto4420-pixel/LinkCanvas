2c45a76399ed2c02a0e5e1cc83002f62bc684533
naoto4420-pixel <naoto4420@gmail.com>
2026-02-18 14:10:23 +0900

    デプロイ用設定

--- chrome_extension/config.js
+++ chrome_extension/config.js
@@ -1,8 +1,8 @@
 // ▼ 開発環境のURL
-const API_BASE_URL = "http://localhost:3000"; 
+//const API_BASE_URL = "http://localhost:3000"; 
 
 // ▼ 本番環境のURL
-//const API_BASE_URL = "https://linkcanvas-kdc7.onrender.com"; 
+const API_BASE_URL = "https://linkcanvas-kdc7.onrender.com"; 
 
 // デプロイ用のデータ作成時には本番URLに書き換える
 export { API_BASE_URL };
